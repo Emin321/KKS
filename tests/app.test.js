f40206fd89f5adb33f071d4052bbb0874fc2eae8
Emin321 <emin.veispahic.23@size.ba>
2026-05-22 15:48:53 +0200

    Izmijenjen app.test.js

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,5 +1,28 @@
-test('basic math test', () => {
-    expect(2 + 2).toBe(4);
+const request = require("supertest");
+const app = require("../app");
+
+describe("Basic routes", () => {
+
+  test("GET / should return status 200", async () => {
+    const response = await request(app).get("/");
+
+    expect(response.statusCode).toBe(200);
+  });
+
+  test("GET / should return correct text", async () => {
+    const response = await request(app).get("/");
+
+    expect(response.text).toBe("test test test");
+  });
+
+  test("GET /about should return status 200", async () => {
+    const response = await request(app).get("/about");
+
+    expect(response.statusCode).toBe(200);
+  });
+
 });
+
+
 
 
